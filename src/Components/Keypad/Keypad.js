4c008a4d54fd2411553960df377f8ad817ca49a5
Lokhtin Denis <denis.lokhtin@gmail.com>
2021-09-09 18:01:20 +0600

    проверка пароля готова

--- src/Components/Keypad/Keypad.js
+++ src/Components/Keypad/Keypad.js
@@ -1,24 +1,53 @@
 import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
 import "./Keypad.css"
 
-const Keypad = (props) => (
-    <div className="keypad">
-        <input className="textarea" placeholder="Код 4524" value="****" readOnly/>
-        <div className="btns">
-            <button>9</button>
-            <button>8</button>
-            <button>7</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button><i className="fas fa-long-arrow-alt-left"/></button>
-            <button>0</button>
-            <button><i className="fas fa-check"/></button>
+const Keypad = (props) => {
+    const dispatch = useDispatch();
+    const currentPassword = useSelector(state => state.currentPassword);
+    const password = useSelector(state => state.password);
+    const addPin = (e) => dispatch({type: 'ADD', payload: e.target.value});
+    const removePin = () => dispatch({type: 'REMOVE'});
+    const clear = () => dispatch({type: 'CLEAR', payload: ''});
+
+    const passwordShown = () => {
+        let value = '';
+        for (let i=0; i < currentPassword.length; i++) {
+           value = value + '*';
+        }
+        return value;
+    };
+
+    const passwordCheck = () => {
+        if (currentPassword.length === 4) {
+            if (currentPassword === password) {
+                clear()
+
+            }
+        }
+    };
+
+    console.log(currentPassword)
+
+    return (
+        <div className="keypad">
+            <input className="textarea" placeholder="Код 4524" value={passwordShown()} readOnly/>
+            <div className="btns">
+                <button value='9' onClick={addPin}>9</button>
+                <button value='8' onClick={addPin}>8</button>
+                <button value='7' onClick={addPin}>7</button>
+                <button value='4' onClick={addPin}>4</button>
+                <button value='5' onClick={addPin}>5</button>
+                <button value='6' onClick={addPin}>6</button>
+                <button value='1' onClick={addPin}>1</button>
+                <button value='2' onClick={addPin}>2</button>
+                <button value='3' onClick={addPin}>3</button>
+                <button onClick={removePin}><i className="fas fa-long-arrow-alt-left"/></button>
+                <button value='0' onClick={addPin}>0</button>
+                <button onClick={passwordCheck}><i className="fas fa-check"/></button>
+            </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default Keypad;
---
+++ src/Store/reducer.js
@@ -0,0 +1,24 @@
+const initialState = {
+    currentPassword: '',
+    password: '4524',
+};
+
+const reducer = (state = initialState, action) => {
+    if (state.currentPassword.length < 4) {
+        if (action.type === 'ADD') {
+            return {...state, currentPassword: state.currentPassword + action.payload}
+        }
+    }
+
+    if (action.type === 'REMOVE') {
+        return {...state, currentPassword: state.currentPassword.substr(0, state.currentPassword.length - 1)}
+    }
+
+    if (action.type === 'CLEAR') {
+        return {...state, currentPassword: ''}
+    }
+
+    return state;
+};
+
+export default reducer;
